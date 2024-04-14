6335991283ff2d6cae98ac627b963005ba6db860
Pavel <harvallen@yandex.ru>
2024-04-14 14:44:34 +0300

    eas

---
+++ eas/test.py
@@ -0,0 +1,20 @@
+import csv
+
+def condense_csv(filename, id_name):
+    with open(filename, 'r', encoding='utf-8') as data, open('condensed.csv', 'w', encoding='utf-8') as rec:
+        text = csv.reader(data)
+        result = dict()
+        title = list()
+        for el in text:
+            num, prop, value = el
+            result.setdefault(num, {}).update({prop:value})
+            if prop not in title:
+                title.append(prop)
+        print(f"{id_name},{','.join(title)}", file=rec)
+        for key, value in result.items():
+            print(f'{key},' + ','.join([v for v in value.values()]), file=rec)
+
+
+
+
+condense_csv('data.csv', id_name='ID')
