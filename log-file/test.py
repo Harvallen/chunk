6f1308db6e1ba4b9bc52083be1cd9f10412d0bb0
Pavel <harvallen@yandex.ru>
2024-04-14 10:40:33 +0300

    ready

--- log-file/test.py
+++ log-file/test.py
@@ -1,10 +1,11 @@
 import csv, datetime
-import os
-print("Текущая рабочая директория:", os.getcwd())
-with open('name_log.csv', 'r', encoding='utf-8') as file, open('new_name_log.csv', 'w', encoding='utf-8'):
+
+with open('name_log.csv', 'r', encoding='utf-8') as file, open('new_name_log.csv', 'w', encoding='utf-8') as rec:
     data = list(csv.reader(file))
     del data[0]
     result = dict()
-    for el in sorted(data, key=lambda x: datetime.strptime(x[-1], '%d/%m/%Y %H:%M')):                         #   sorted by datetime
+    for el in sorted(data, key=lambda x: datetime.datetime.strptime(x[-1], '%d/%m/%Y %H:%M')):                         #   sorted by datetime
         username, email, dt = el
-        result[email] = username
+        result[email] = (username, dt)
+    print(f'username,email,dtime', file=rec)
+    [print(f'{v[0]},{k},{v[1]}', file=rec) for k, v in sorted(result.items())]                                         #   sorted by keys
