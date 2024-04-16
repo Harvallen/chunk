e044514296e9cd5d860ee1f1b494f1d41e3a364f
Pavel <harvallen@yandex.ru>
2024-04-17 00:03:50 +0300

    done

--- playground/test.py
+++ playground/test.py
@@ -4,7 +4,7 @@ with open('playgrounds.csv', 'r', encoding='utf-8') as file, open('addresses.jso
     del data[0]
     result = dict()
     for ObjectName, AdmArea, District, Address in data:
-        result[AdmArea] = result.setdefault(AdmArea, {}) | {District: Address}      # переделать в список всех адресов в этом районе.
-    
+        result[AdmArea] = result.setdefault(AdmArea, {})      # переделать в список всех адресов в этом районе.
+        result[AdmArea].setdefault(District, []).append(Address)
     json.dump(result, rec, indent=3, ensure_ascii=False)
     
