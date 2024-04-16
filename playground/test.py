fd0d48f5546915e19e62aeae1dce6fb400a3c375
Pavel <harvallen@yandex.ru>
2024-04-16 23:43:40 +0300

    3

---
+++ playground/test.py
@@ -0,0 +1,10 @@
+import csv, json
+with open('playgrounds.csv', 'r', encoding='utf-8') as file, open('addresses.json', 'w', encoding='utf-8') as rec:
+    data = list(csv.reader(file, delimiter=';'))
+    del data[0]
+    result = dict()
+    for ObjectName, AdmArea, District, Address in data:
+        result[AdmArea] = result.setdefault(AdmArea, {}) | {District: Address}      # переделать в список всех адресов в этом районе.
+    
+    json.dump(result, rec, indent=3, ensure_ascii=False)
+    
